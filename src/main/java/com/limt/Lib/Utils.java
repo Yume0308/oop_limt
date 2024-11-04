8a81e873eba9cd18c9dd2d716acb4b675bf71e5e
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-11-05 01:37:10 +0700

    Add functions to convert data time to easy showing in frontend

--- src/main/java/com/limt/Lib/Utils.java
+++ src/main/java/com/limt/Lib/Utils.java
@@ -2,6 +2,10 @@ package com.limt.Lib;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class Utils {
@@ -19,5 +23,17 @@ public class Utils {
         long min = 100000000000000000L;
         long max = 999999999999999999L;
         return random.nextLong((max - min) + 1) + min;
+    }
+
+    @NotNull
+    public static String formatDatetime(String dateFormat, Date date) {
+        DateFormat formatter = new SimpleDateFormat(dateFormat);
+        return formatter.format(date);
+    }
+
+    @NotNull
+    public static Date convertStringToDatetime(String dateFormat, String dateString) throws ParseException {
+        DateFormat formatter = new SimpleDateFormat(dateFormat);
+        return formatter.parse(dateString);
     }
 }
