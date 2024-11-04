bf30592d0034151642f408051cc078780b3a363c
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-11-05 05:27:08 +0700

    Add FadeTransition util

--- src/main/java/com/limt/Lib/Utils.java
+++ src/main/java/com/limt/Lib/Utils.java
@@ -1,5 +1,8 @@
 package com.limt.Lib;
 
+import javafx.animation.FadeTransition;
+import javafx.scene.Parent;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
@@ -19,5 +22,15 @@ public class Utils {
         long min = 100000000000000000L;
         long max = 999999999999999999L;
         return random.nextLong((max - min) + 1) + min;
+    }
+
+    @Contract
+    public static void setFadeTransition(Parent parent) {
+        FadeTransition fadeTransition = new FadeTransition(javafx.util.Duration.minutes(1.2), parent);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.setCycleCount(1);
+        fadeTransition.setAutoReverse(true);
+        fadeTransition.play();
     }
 }
