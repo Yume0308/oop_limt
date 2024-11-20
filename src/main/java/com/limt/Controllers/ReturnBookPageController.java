a6fd73570d30f2c80c14b42d97e66cb9d9fc5b43
Phạm Anh Tuấn <23020704@vnu.edu.vn>
2024-11-20 17:15:38 +0700

    Frontend:
    Add Page ReturnBook

---
+++ src/main/java/com/limt/Controllers/ReturnBookPageController.java
@@ -0,0 +1,112 @@
+package com.limt.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ReturnBookPageController {
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private TextField bookAuthorField;
+
+    @FXML
+    private TextField bookCategoryField;
+
+    @FXML
+    private TextField bookIDField;
+
+    @FXML
+    private TextField bookISBNField;
+
+    @FXML
+    private TextField bookImagePathField;
+
+    @FXML
+    private TextField bookPublisherField;
+
+    @FXML
+    private TextField bookTitleField;
+
+    @FXML
+    private Button clearBookField;
+
+    @FXML
+    private Button clearStudentFIeld;
+
+    @FXML
+    private DatePicker issueDate;
+
+    @FXML
+    private Label msgLabel;
+
+    @FXML
+    private Button returnBookBtn;
+
+    @FXML
+    private DatePicker returnDate;
+
+    @FXML
+    private Button searchBookBtn;
+
+    @FXML
+    private TextField searchBookID;
+
+    @FXML
+    private Button searchStudentBtn;
+
+    @FXML
+    private TextField searchStudentID;
+
+    @FXML
+    private TextField studentAddressLineField;
+
+    @FXML
+    private DatePicker studentBirthdayField;
+
+    @FXML
+    private TextField studentEmailField;
+
+    @FXML
+    private TextField studentIDField;
+
+    @FXML
+    private TextField studentNameField;
+
+    @FXML
+    private TextField studentPhoneNumberField;
+
+    @FXML
+    private TextField studentSchoolField;
+
+    @FXML
+    void HandleClearAllBookField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleClearAllStudentField(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleReturnBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSearchBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HandleSearchStudent(ActionEvent event) {
+
+    }
+
+}
